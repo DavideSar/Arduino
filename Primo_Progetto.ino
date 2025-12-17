2b81d14f152883589236f3af7e57f376fa25ad82
DavideSar <davide.sargiacomo@gmail.com>
2025-12-17 15:02:04 +0100

    SOS

--- Primo_Progetto.ino
+++ Primo_Progetto.ino
@@ -1,13 +1,52 @@
-// the setup function runs once when you press reset or power the board
+int led = 4;
+int delayDash = 1500;
+int delayDot = 500;
+int delaySpento = 500;
+
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(led, OUTPUT);
 }
 
-// the loop function runs over and over again forever
+
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(100);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(100);                      // wait for a second
+  //S
+  digitalWrite(led, HIGH);
+  delay(delayDash);
+  digitalWrite(led, LOW);
+  delay(delaySpento);
+  digitalWrite(led, HIGH);
+  delay(delayDash);
+  digitalWrite(led, LOW);
+  delay(delaySpento);
+  digitalWrite(led, HIGH);
+  delay(delayDash);
+  digitalWrite(led, LOW);
+  delay(delaySpento);
+  //O
+  digitalWrite(led, HIGH);
+  delay(delayDot);
+  digitalWrite(led, LOW);
+  delay(delaySpento);
+  digitalWrite(led, HIGH);
+  delay(delayDot);
+  digitalWrite(led, LOW);
+  delay(delaySpento);
+  digitalWrite(led, HIGH);
+  delay(delayDot);
+  digitalWrite(led, LOW);
+  delay(delaySpento);
+  //S
+  digitalWrite(led, HIGH);
+  delay(delayDash);
+  digitalWrite(led, LOW);
+  delay(delaySpento);
+  digitalWrite(led, HIGH);
+  delay(delayDash);
+  digitalWrite(led, LOW);
+  delay(delaySpento);
+  digitalWrite(led, HIGH);
+  delay(delayDash);
+  digitalWrite(led, LOW);
+  delay(delaySpento);
 }
